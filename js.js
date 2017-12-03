900d9268b3131d39d2045a37f1aa8a41efb34a72
Mick Jasinski <m.jasinski@minimalcraft.net>
2017-12-03 21:17:51 +0000

    BetterNow

--- js.js
+++ js.js
@@ -3,11 +3,6 @@ var btnLength = btn.length;
 
 function btnloop(btn) {
     for (var a = 0; a < btnLength; a++) {
-        alert(btn[0].innerText);
-        alert(btn[1].innerText);
-        alert(btn[2].innerText);
-        alert(btn[3].innerText);
-        alert(btn[4].innerText);
-        break;
+        alert(btn[a].innerText);
     }
 }
